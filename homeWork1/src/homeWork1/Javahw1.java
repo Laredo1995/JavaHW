f2fa639660ddfe2ea821516d32f5e7d9237555c5
Laredo1995 <pablo.lared@snhu.edu>
2023-04-03 18:53:37 -0400

    Java HM

---
+++ homeWork1/src/homeWork1/Javahw1.java
@@ -0,0 +1,27 @@
+package homeWork1;
+import java.util.Scanner;
+
+public class Javahw1 {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		// TODO Auto-generated method stub
+		int number=5;
+	System.out.print("Guss a number ");
+	int guess=Integer.parseInt(input.nextLine());
+	if(guess>10) {
+    System.out.printf("You guessed %d and that is wrong",guess);
+	String notAString=7;
+	
+	System.out.println(notAString;
+    	
+	
+}
+int interger=7;
+System.out.print(interger);
+
+int testWontWork="hello";
+
+
+}	
+}
